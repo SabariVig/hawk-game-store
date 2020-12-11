2addf71c295a822809b657db0644bde80ef2c5bc
SabariVig <SabariVig@users.noreply.github.com>
2020-12-11 18:03:08 +0000

    deploy: f995e4a152b43820188f883d8badba2f44f4c3a1

---
+++ precache-manifest.04d96d3b7be6037991ad9e83daec2474.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dc24246757d7abb12e92661ff3c9e4da",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ac808185bc55fd777b8b",
+    "url": "/static/css/main.34df4a0d.chunk.css"
+  },
+  {
+    "revision": "801a2c9e3ee06d870903",
+    "url": "/static/js/2.307a14ec.chunk.js"
+  },
+  {
+    "revision": "ac808185bc55fd777b8b",
+    "url": "/static/js/main.83defd3b.chunk.js"
+  },
+  {
+    "revision": "db15b2c96918a3564965",
+    "url": "/static/js/runtime~main.61660e35.js"
+  }
+]);
