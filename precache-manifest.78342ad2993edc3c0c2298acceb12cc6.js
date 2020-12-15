d099a4650a37a155bf8a78f8080527252e76b7aa
SabariVig <SabariVig@users.noreply.github.com>
2020-12-15 10:22:32 +0000

    deploy: 6ca43aa92bd5116855d5d82da843f830c7c51190

--- precache-manifest.78342ad2993edc3c0c2298acceb12cc6.js
+++ precache-manifest.78342ad2993edc3c0c2298acceb12cc6.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "a9e7bea7105bfc141901513e95c3cad8",
+    "revision": "f769cb67d452cb65fb6c29882f31f9ba",
     "url": "/index.html"
   },
   {
-    "revision": "1c55ae0014343fd5f5bb",
+    "revision": "de2dab3e8a345f93c37f",
     "url": "/static/css/main.34df4a0d.chunk.css"
   },
   {
@@ -12,8 +12,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/js/2.d7f4bd54.chunk.js"
   },
   {
-    "revision": "1c55ae0014343fd5f5bb",
-    "url": "/static/js/main.e8e07d08.chunk.js"
+    "revision": "de2dab3e8a345f93c37f",
+    "url": "/static/js/main.0ea1cec6.chunk.js"
   },
   {
     "revision": "db15b2c96918a3564965",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.62a105b0e2a333f488bf757cb987b4e8.js"
+  "/precache-manifest.78342ad2993edc3c0c2298acceb12cc6.js"
 );
 
 self.addEventListener('message', (event) => {
